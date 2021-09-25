bc2ec9a8f49d77252edba84c8ffe9cd9d1c3082e
KomlaRD <ankueric1@gmail.com>
2021-09-25 16:58:50 +0000

    code: Packages setup code

---
+++ packages.R
@@ -0,0 +1,20 @@
+#Packages setup
+###############
+
+pacman::p_load (
+        tidyverse, # Data management
+        rio, # Importing dataset
+        here, # Working directory
+        janitor, # Clean data names
+        googlesheets4, # Read sheets from Google sheets
+        gtsummary, # Create summary tables
+        skimr, # Overview of dataset
+        GGally, # Correlation matrix
+        tinytex, # Creating pdf documents
+        finalfit, # Creating models
+        ggthemes, # Plot themes
+        ggstatsplot, # Inferential plots
+        knitr # Chunk options
+        
+        
+)
